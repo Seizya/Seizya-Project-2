21d0c395fa1d4227ad329944276795989ddd5cdf
八雲 正邪 <30519243+Seizya@users.noreply.github.com>
2018-07-26 15:50:06 +0900

    Add files via upload

--- Main menu/main menu.js
+++ Main menu/main menu.js
@@ -1,9 +1,25 @@
 var scroll = document.getElementById("scroll");
-
-scroll.addEventListener('mouseenter', () => {
-    document.getElementById("scroll").className = "hover";
+var clicka = false;
+scroll.addEventListener('click', () => {
+    clicka = !clicka;
+    console.log(clicka)
+    if (clicka) {
+        document.getElementById("scroll").className = "hover";
+    } else {
+        document.getElementById("scroll").className = "out";
+    }
 }, false);
 
-scroll.addEventListener('mouseleave', () => {
-    document.getElementById("scroll").className = "out";
-}, false)
+var menu = document.getElementById("menu");
+var clickb = false;
+menu.addEventListener('click', () => {
+    clickb = !clickb;
+    console.log(clickb)
+    if (clickb) {
+        document.getElementById("menu").className = "hover";
+        document.getElementById("menus").className = "hover";
+    } else {
+        document.getElementById("menu").className = "";
+        document.getElementById("menus").className = "";
+    }
+}, false);
